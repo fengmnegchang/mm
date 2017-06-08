b9c8b045a2be03a63ab720d67a2cc59f0d68084c
fengmnegchang <624926379@qq.com>
2017-06-08 10:23:10 +0800

    m leftmenu

---
+++ mm/src/com/open/mm/bean/m/MSlideMenuBean.java
@@ -0,0 +1,44 @@
+/**
+ *****************************************************************************************************************************************************************************
+ * 
+ * @author :fengguangjing
+ * @createTime:2017-6-8上午10:11:23
+ * @version:4.2.4
+ * @modifyTime:
+ * @modifyAuthor:
+ * @description:
+ *****************************************************************************************************************************************************************************
+ */
+package com.open.mm.bean.m;
+
+import com.open.android.bean.CommonBean;
+
+/**
+ *****************************************************************************************************************************************************************************
+ * 
+ * @author :fengguangjing
+ * @createTime:2017-6-8上午10:11:23
+ * @version:4.2.4
+ * @modifyTime:
+ * @modifyAuthor:
+ * @description:
+ *****************************************************************************************************************************************************************************
+ */
+public class MSlideMenuBean extends CommonBean {
+	private String href;
+	private String title;
+	public String getHref() {
+		return href;
+	}
+	public void setHref(String href) {
+		this.href = href;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+
+}
