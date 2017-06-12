88508cfdbbc13bbf1c2a15fe2562b126290d39d4
fengmnegchang <624926379@qq.com>
2017-06-12 15:25:48 +0800

    titlebar事件

--- mm/src/com/open/mm/activity/app/MCollectionGridFragmentActivity.java
+++ mm/src/com/open/mm/activity/app/MCollectionGridFragmentActivity.java
@@ -14,6 +14,7 @@ package com.open.mm.activity.app;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
+import android.view.View;
 
 import com.open.mm.R;
 import com.open.mm.activity.m.MCommonTitleBarActivity;
@@ -40,14 +41,42 @@ public class MCollectionGridFragmentActivity extends MCommonTitleBarActivity{
 	@Override
 	protected void initValue() {
 		// TODO Auto-generated method stub
-//		super.initValue();
+		super.initValue();
 		if (getIntent().getStringExtra("URL") != null) {
 			url = getIntent().getStringExtra("URL");
 		} else {
 			url = UrlUtils.MM_M;
 		}
 		addfragment();
+		
+		setCenterTextValue("我的收藏");
+		setRightTextValue("编辑");
+		setRightTextVisivable(true);
+		setLeftImageResId(R.drawable.left01);
+		setLeftTextVisivable(false);
 	}
+	
+	
+	/* (non-Javadoc)
+	 * @see com.open.mm.activity.m.MCommonTitleBarActivity#onClick(android.view.View)
+	 */
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.txt_right:
+			
+			break;
+		case R.id.id_iv_left:
+			finish();
+			break;
+		default:
+			super.onClick(v);
+			break;
+		}
+	}
+
+
 	/*
 	 * (non-Javadoc)
 	 * 
